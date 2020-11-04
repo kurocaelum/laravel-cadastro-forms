dee32cda49778293cbb40626dbbac96066fc8902
Victor <kuro_caelum@outlook.com>
2020-11-04 14:01:38 -0300

    first commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Cadastro de Produtos</title>
+    <style>
+        body {
+            padding: 20px;
+        }
+
+        .navbar {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        @component('component_navbar', ["current" => $current])
+        @endcomponent
+
+        <main role="main">
+            @hasSection ('body')
+                @yield('body')
+            @endif
+        </main>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
